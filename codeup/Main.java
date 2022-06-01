dfae9153783898cf7626681f3bc35fe3d6f317b0
Park Junsu <krpjs0508@gmail.com>
2022-06-01 14:31:35 +0900

    20220601 자바
    코드업

---
+++ codeup/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Main {
+    //1001
+    /*public static void main(String[] args) {
+        System.out.println("Hello");
+    }*/
+    //1002
+    /*public static void main(String[] args) {
+        System.out.println("Hello"+" "+"World");
+    }*/
+    //1003
+    /*public static void main(String[] args) {
+        System.out.println("Hello\nWorld");
+    }*/
+    //1004
+    /*public static void main(String[] args) {
+        System.out.println("\'Hello\'");
+    }*/
+    //1005
+    /*public static void main(String[] args) {
+        System.out.println("\"Hello World\"");
+    }*/
+    //1006
+    /*public static void main(String[] args) {
+        System.out.println("\"!@#$%^&*()\"");
+    }*/
+    //1007
+    /*public static void main(String[] args) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }*/
+    //1008
+    /*public static void main(String[] args) {
+        System.out.println("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518");
+    }*/
+    //1010
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        System.out.printf("%d", n);
+    }*/
+    //1011
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        char n = sc.next().charAt(0);
+        System.out.println(n);
+    }*/
+    //1012
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        float n = sc.nextFloat();
+        System.out.printf("%.6f", n);
+    }*/
+    //1013
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.printf("%d %d", n, m);
+    }*/
+    //1014
+
+}
