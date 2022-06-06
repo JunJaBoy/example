4c6bcbea271840d5b45640fa96695126f4db2956
Park Junsu <krpjs0508@gmail.com>
2022-06-06 20:35:07 +0900

    20220606 자바
    백준

---
+++ baekjoon/Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Main {
+    //2557
+    /*public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }*/
+    //10718
+    /*public static void main(String[] args) {
+        System.out.println("강한친구 대한육군\n강한친구 대한육군");
+    }*/
+    //10171
+    /*public static void main(String[] args) {
+        System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+    }*/
+    //10172
+    /*public static void main(String[] args) {
+        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+    }*/
+    //1000
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d", a+b);
+    }*/
+    //1001
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a - b);
+    }*/
+    //10998
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a * b);
+    }*/
+    //1008
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(a / b);
+    }*/
+    //10869
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a / b);
+        System.out.println(a%b);
+    }*/
+    //10926
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        String id= sc.nextLine();
+        System.out.println(id+"??!");
+    }*/
+    //18108
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        int a = sc.nextInt();
+        System.out.println(a-543);
+    }*/
+    //10430
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println((a+b)%c+"\n"+((a%c) + (b%c))%c+"\n"+(a*b)%c+"\n"+((a%c)*(b%c))%c);
+    }
+}
