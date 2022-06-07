506e5d85352b4c5f9224ce08abfceeac72ac8471
Park Junsu <krpjs0508@gmail.com>
2022-06-07 21:51:09 +0900

    20220607 자바
    백준

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -69,11 +69,48 @@ public class Main {
         System.out.println(a-543);
     }*/
     //10430
-    static Scanner sc = new Scanner(System.in);
+    /*tatic Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
         System.out.println((a+b)%c+"\n"+((a%c) + (b%c))%c+"\n"+(a*b)%c+"\n"+((a%c)*(b%c))%c);
-    }
+    }*/
+    //2588
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt(); //첫번째 수 입력(첫번째 수는 자릿수로 나눌 필요 X)
+        String b = sc.next(); //두번째 수 배열로 입력(자릿수 나누기 위해)
+
+        System.out.println(a*(b.charAt(2) - '0')); //첫번째 수에 b의 3번째 자리수 곱하기(charAt으로 각 자리수를 배열에서 문자열로 변환 후 아스키코드 번호 48번을 빼서 정수로 만들기(0 = 아스키코드 48))
+        System.out.println(a*(b.charAt(1) - '0')); //두번째 수에 b의 2번째 자리수 "
+        System.out.println(a*(b.charAt(0) - '0')); //두번째 수에 b의 1번째 자리수 "
+        System.out.println(a*Integer.parseInt(b)); //전체 수를 곱한 값 출력(parseInt로 배열 b를 정수로 바꿈)
+    }*/
+    //25083
+    /*public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }*/
+    //1330
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a > b){
+            System.out.println(">");
+        }
+        else if (b < a) {
+            System.out.println("<");
+        }
+        else{
+            System.out.println("=");
+        }
+        sc.close();
+    }*/
+
 }
