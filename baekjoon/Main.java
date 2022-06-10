0070f240c79c9e2684536ea6354c1ca622c49c09
Park Junsu <krpjs0508@gmail.com>
2022-06-10 23:28:38 +0900

    20220610 자바
    백준

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    //1
     //2557
     /*public static void main(String[] args) {
         System.out.println("Hello World!");
@@ -96,21 +97,117 @@ public class Main {
         System.out.println("      |");
         System.out.println("      |");
     }*/
+    //2
     //1330
     /*static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         int a = sc.nextInt();
         int b = sc.nextInt();
+
         if (a > b){
             System.out.println(">");
         }
-        else if (b < a) {
+        else if (a < b) {
             System.out.println("<");
         }
-        else{
-            System.out.println("=");
+        else {
+            System.out.println("==");
         }
-        sc.close();
     }*/
+    //9498
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int score = sc.nextInt();
+        if (score >= 90) {
+            System.out.println("A");
+        }
+        else if (score >= 80) {
+            System.out.println("B");
+        }
+        else if (score >= 70) {
+            System.out.println("C");
+        }
+        else if (score >= 60) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }*/
+    //2753
+    /*static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int y = sc.nextInt();
+        if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0)) {
+            System.out.println("1");
+        }
+        else System.out.println("0");
+    }*/
+    //14681
+    /*static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x > 0) {
+            if (y > 0) {
+                System.out.println("1");
+            } else System.out.println("4");
+        } else {
+            if (y > 0) {
+                System.out.println("2");
+            } else System.out.println("3");
+        }
+    }*/
+    //2884
+    /*static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        if (m < 45) {
+            h--;
+            m = 60 - (45 - m);
+            if (h < 0) {
+                h = 23;
+            }
+            System.out.println(h + " " + m);
+        } else {
+            System.out.println(h + " " + (m - 45));
+
+        }
+    }*/
+    //2525
+    /*static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        h = h * 60 + m + n;
+        m = (h % 60);
+        h /= 60;
+        if (h > 23){h -=24;}
+        System.out.println(h + " " + m);
+    }*/
+    //2480++_+_+_+_+안풀었음
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] n = new int[3];
+        int cnt = 0;
+        for (int i=0; i < 3; i ++) {
+            n[i] = Integer.parseInt(sc.next());
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (n[i] - n[j] == 0) {
+                    cnt++;
+                } else {
+                    break;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
 }
